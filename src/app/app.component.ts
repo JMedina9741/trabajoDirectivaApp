302566b5d77c03d7adcb796b2511aacbca2ad71f
JMedina9741 <147419480+JMedina9741@users.noreply.github.com>
2023-11-16 18:07:38 +0100

    first-commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ServiciosEmpleadosService } from './servicios-empleados.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'trabajoDirectivaApp';
+  today: Date;
+  titulo = 'Registro de usuarios';
+  mensaje = "";
+  registrado = false;
+  nombre:string="";
+  apellido:string="";
+  entradas:Array<{titulo:string}>;
+  cargo:string="";
+
+constructor(private miServicio:ServiciosEmpleadosService){
+  this.today = new Date();
+ 
+   this.entradas=[
+    {titulo:"Mi primera entrada de blog"},
+    {titulo:"Mi segunda entrada de blog"},
+    {titulo:"Mi tercera entrada de blog"},
+    {titulo:"Mi cuarta entrada de blog"}
+  ] 
+}
+
+
+
+// Funcion de registro de usuario
+
+registrarUsuario(){
+  
+  this.registrado = true;
+  this.miServicio.muestraMensaje("He incorporado mi servicio");
+  this.mensaje = "Usuario registrado correctamente";
+  
+}
+
 }
---
+++ src/app/servicios-empleados.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ServiciosEmpleadosService {
+
+  constructor() { }
+  muestraMensaje(mensaje:string){
+    alert(mensaje);
+  }
+}
